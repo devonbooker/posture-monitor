16e682de41d931f3105f6868a8acf07dcddf3922
Devon Booker <devonbookerofficial@gmail.com>
2026-04-22 19:01:38 -0500

    Test: trivial description tweak to exercise terraform-plan PR workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "server_ipv4" {
   value       = hcloud_server.app.ipv4_address
-  description = "Public IPv4 of the app server"
+  description = "Public IPv4 of the app server (reachable from the internet)"
 }
 
 output "server_ipv6" {
